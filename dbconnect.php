1b918c6aba2feeb0232c02d4c365a9c8cd496dbf
kiuthy229 <68453603+kiuthy229@users.noreply.github.com>
2020-10-08 11:02:14 +0700

    ATN
    abc

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-$server="localhost";
-$username="postgres";
-$password="123456";
-$database="test";
+$server="ec2-3-228-114-251.compute-1.amazonaws.com";
+$username="pkwdvbhlbaoixq";
+$password="0a3d318256b2ed64ac31827a3d9a5e863d5cbefde715c75aa4c6edbcd4aadf15";
+$database="d4f8b7fakijqp2";
 $connString="host=$server dbname=$database user=$username password=$password";
 ?>
